f74d89a72922310aef7ec7a212c359e5e4ad2a8f
Alejandro Sanchez <alejandro.sanchez@cern.ch>
2023-12-03 22:13:46 -0600

    WIP. Working version of conversion script

--- convertNanoToHDF5.py
+++ convertNanoToHDF5.py
@@ -1,15 +1,31 @@
 """
-Gets DeepMETv1 training inputs from PFNano NanoAOD file and saves them to HDF5
-output.
-Usage:
-    python convertNanoToHDF5.py -i/--input <file.root> -o/--output <file.h5>
-                                [-p/--npf <num>] [--auto_npf]
-Help:
-    python convertNanoToHDF5.py -h/--help
+Gets DeepMETv1 training data from PFNano NanoAOD file and saves them to HDF5 output.
+
+Help Message
+------------
+usage: convertNanoToHDF5.py [-h] [-v] [-l <leptons>] [-p <npf>] [--auto_npf] [-f <fill>] <inputfile> <outputfile>
+
+positional arguments:
+  <inputfile>           input NanoAOD file.
+  <outputfile>          output HDF5 file
+
+options:
+  -h, --help            show this help message and exit
+  -v, --verbose         print logs
+  -l <leptons>, --leptons <leptons>
+                        number of leptons to remove from pfcands (default is 2)
+  -p <npf>, --npf <npf>
+                        max number of pfcands per event (default is 4500)
+  --auto_npf            determine npf based on input events max npf (overrides -p/--npf)
+  -f <fill>, --fill <fill>
+                        value used to pad and fill empty training data entries (default is -999)
 """
+# Native libraries
 import sys
+import logging
 import warnings
 import argparse
+# Third-party libraries
 import numpy as np
 import awkward as ak
 import h5py
@@ -17,11 +33,22 @@ from coffea.nanoevents import NanoEventsFactory
 from coffea.nanoevents.schemas import NanoAODSchema
 
 def get_args():
+    """Get command line arguments"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', help='Input NanoAOD file', metavar='<file>', required=True, type=str)
-    parser.add_argument('-o', '--output', help='Output HDF5 file', metavar='<file>', required=True, type=str)
-    parser.add_argument('-p', '--npf', help='Max number of PF candidates per event. Default is 4500', metavar='<num>', default=4500, type=int)
-    parser.add_argument('--auto_npf', help='Determine npf based on input events max npf. Overrides -p/--npf', action='store_true')
+    parser.add_argument('inputfile', metavar='<inputfile>', type=str,
+        help='input NanoAOD file.')
+    parser.add_argument('outputfile', metavar='<outputfile>', type=str,
+        help='output HDF5 file')
+    parser.add_argument('-v', '--verbose', action='store_true',
+        help='print logs')
+    parser.add_argument('-l', '--leptons', metavar='<leptons>', type=int, default=2,
+        help='number of leptons to remove from pfcands (default is 2)')
+    parser.add_argument('-p', '--npf', metavar='<npf>', type=int, default=4500,
+        help='max number of pfcands per event (default is 4500)')
+    parser.add_argument('--auto_npf', action='store_true',
+        help='determine npf based on input events max npf (overrides -p/--npf)')
+    parser.add_argument('-f', '--fill', metavar='<fill>', type=float, default=-999,
+        help='value used to pad and fill empty training data entries (default is -999)')
     return parser.parse_args()
 
 def delta_phi(obj1, obj2):
@@ -34,8 +61,19 @@ def delta_r(obj1, obj2):
     dphi = delta_phi(obj1, obj2)
     return np.hypot(deta, dphi)
 
-def remove_match(pfcands, lepton, r_max=0.001):
-    """Remove deltaR matched lepton from pfcand list"""
+def px(obj):
+    """Returns object px"""
+    return obj.pt * np.cos(obj.phi)
+
+def py(obj):
+    """Returns object py"""
+    return obj.pt * np.sin(obj.phi)
+
+def remove_lepton(pfcands, lepton, r_max=0.001):
+    """
+    Remove deltaR matched lepton from pfcands. A lepton is matched to the
+    nearest pfcand if they are closer than a deltaR of r_max.
+    """
     dr = delta_r(pfcands, lepton)
     ipf = ak.local_index(dr)
     imin = ak.argmin(dr, axis=-1)
@@ -44,12 +82,8 @@ def remove_match(pfcands, lepton, r_max=0.001):
     return pfcands[np.invert(is_match)]
 
 def main():
-    args = get_args()
-    # Paramters
-    n_lep = 2                       # num leptons matched/removed from pfcands
-    pad_value = 0                   # padding for empty training data
-    output_fields = ['px', 'py']    # GenMET quantities saved as outputs
-    input_fields = [                # PFCands quantities saved as inputs
+    """main"""
+    input_fields = [
         'd0',
         'dz',
         'eta',
@@ -62,77 +96,78 @@ def main():
         'charge',
         #'fromPV',
     ]
+    output_fields = ['px', 'py']
+    args = get_args()
+    if args.verbose:
+        logging.basicConfig(format='%(levelname)s[%(asctime)s]:%(message)s',
+                            #datefmt='%Y-%m-%d %H:%M:%S',
+                            level=logging.INFO)
 
-    print('Fetching events')
+    logging.info('Fetching events')
     events = NanoEventsFactory.from_root(
-        args.input,
+        args.inputfile,
         schemaclass=NanoAODSchema
     ).events()
-    print('Num events before selection:', len(events))
-    n_lep_all = ak.num(events.Muon) + ak.num(events.Electron)
-    events = events[n_lep_all >= n_lep]
-    print('Num events after selection: ', len(events))
 
-    # Get pf cands and leading leptons
+    logging.info(f'Num events before selection: {len(events)}')
+    n_lep = ak.num(events.Muon) + ak.num(events.Electron)
+    events = events[n_lep >= args.leptons]
+    logging.info(f'Num events after selection:  {len(events)}')
+
     pfcands = events.PFCands
+    genMET = events.GenMET
     leptons = ak.concatenate([events.Muon, events.Electron], axis=1)
     leptons = leptons[ak.argsort(leptons.pt, axis=-1, ascending=False)]
-    leptons = leptons[:,:n_lep]
+    leptons = leptons[:,:args.leptons]
 
-    for ilep in range(n_lep):
-        print(f'DeltaR matching lepton {ilep+1}')
-        pfcands = remove_match(pfcands, leptons[:,ilep])
+    logging.info('Removing leptons from pfcand list')
+    for ilep in range(args.leptons):
+        pfcands = remove_lepton(pfcands, leptons[:,ilep])
 
-    print('Computing PFCands_px')
-    pfcands['px'] = pfcands.pt * np.cos(pfcands.phi)
-    print('Computing PFCands_py')
-    pfcands['py'] = pfcands.pt * np.cos(pfcands.phi)
+    logging.info('Computing px and py')
+    pfcands['px'] = px(pfcands)
+    pfcands['py'] = py(pfcands)
+    genMET['px'] = px(genMET)
+    genMET['py'] = py(genMET)
 
-    print('Preparing training input data:')
-    training_inputs = []
-    fields = ak.unzip(pfcands[input_fields])
+    # Training input data
+    logging.info('Preparing training inputs')
+    X = []
+    pfcands_fields = ak.unzip(pfcands[input_fields])
     npf = args.npf if not args.auto_npf else ak.max(ak.num(pfcands))
-    for i,field in enumerate(fields):
-        print(f'Processing PFCands_{input_fields[i]}')
+    for i,field in enumerate(pfcands_fields):
+        logging.info(f'Processing PFCands_{input_fields[i]}')
         field = ak.pad_none(field, npf, axis=-1, clip=True)
-        field = ak.fill_none(field, pad_value)
-        training_inputs.append(field)
+        field = ak.fill_none(field, args.fill)
+        X.append(field)
+    X = np.array(X)
+    logging.info(f'Training inputs shape: {np.shape(X)}') # (nfields,nevents,npf)
 
-    # Training input format: training_inputs[ifield][ievt][ipf]
-    training_inputs = np.array(training_inputs)
+    # Training output data
+    logging.info('Preparing training outputs')
+    genMET_fields = ak.unzip(genMET[output_fields])
+    Y = np.array(genMET_fields)
+    logging.info(f'Training outputs: {np.shape(Y)}') # (nfields,nevents)
 
-    print(np.shape(training_inputs))
-    sys.exit('DEBUG')
+    with h5py.File(args.outputfile, 'w') as h5f:
+        h5f.create_dataset('X',    data=X,    compression='lzf')
+        h5f.create_dataset('Y',    data=Y,    compression='lzf')
 
-    #print('Saving training outputs')
-    #genMET_outputs = ak.concatenate([
-    #   [events.GenMET.pt * np.cos(events.GenMET.phi)]       # px
-    #   [events.GenMET.pt * np.sin(events.GenMET.phi)]       # py
-    #], axis=1)
-
-#    # Dictionaries to assign labels to discrete values
-#    d_encoding = {
-#       b'PFCands_charge':{-1.0: 0, 0.0: 1, 1.0: 2},
-#       b'PFCands_pdgId':{-211.0: 0, -13.0: 1, -11.0: 2, 0.0: 3, 1.0: 4, 2.0: 5, 11.0: 6, 13.0: 7, 22.0: 8, 130.0: 9, 211.0: 10},
-#    }
-#
-#        # truth info
-#        Y[e][0] += tree['GenMET_pt'][e] * np.cos(tree['GenMET_phi'][e])
-#        Y[e][1] += tree['GenMET_pt'][e] * np.sin(tree['GenMET_phi'][e])
-#
-#    with h5py.File(args.output, 'w') as h5f:
-#        h5f.create_dataset('X',    data=X,    compression='lzf')
-#        h5f.create_dataset('Y',    data=Y,    compression='lzf')
-#        h5f.create_dataset('EVT',  data=EVT,  compression='lzf')
-#        h5f.create_dataset('XLep', data=XLep, compression='lzf')
+    # Dictionaries to assign labels to discrete values
+    #d_encoding = {
+    #   b'PFCands_charge':{-1.0: 0, 0.0: 1, 1.0: 2},
+    #   b'PFCands_pdgId':{-211.0: 0, -13.0: 1, -11.0: 2, 0.0: 3,
+    #                      1.0: 4, 2.0: 5, 11.0: 6, 13.0: 7,
+    #                      22.0: 8, 130.0: 9, 211.0: 10},
+    #}
 
 if __name__ == '__main__':
     try:
         # Suppress irrelevant warnings from coffea. Warnings have to do with
         # the naming convention of some branches not relevant to DeepMET.
-        # 'Jet_*' and 'FatJet_*'
-        with warnings.catch_warnings():
-            warnings.filterwarnings('ignore', message='Found duplicate branch .*Jet_')
-            main()
+        # The offending branches are 'Jet_*' and 'FatJet_*'.
+        warnings.filterwarnings('ignore',
+                                message='Found duplicate branch .*Jet_')
+        main()
     except KeyboardInterrupt:
         sys.exit('\nStopping early.')

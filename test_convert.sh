89dd6210e5a6f67e007c024167892fa535b0f7eb
Alejandro Sanchez <alejandro.sanchez@cern.ch>
2023-12-03 21:19:18 -0600

    Update convert test script

--- test_convert.sh
+++ test_convert.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python convertNanoToHDF5.py -i /uscms_data/d3/alejands/DeepMET/NanoAOD/DYJetsToLL_Run3Summer22_NanoAODv12_fromMiniAODv4/out_deepmet_1-1.root -o test.h5
+python convertNanoToHDF5.py -v /uscms_data/d3/alejands/DeepMET/NanoAOD/DYJetsToLL_Run3Summer22_NanoAODv12_fromMiniAODv4/out_deepmet_1-1.root test.h5
